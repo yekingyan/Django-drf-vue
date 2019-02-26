930f7951da9cc05e07478a0c96474c5f87850af7
yekingyan <529616@gmail.com>
2019-02-26 22:03:09 +0800

    交易的model 设计

--- demo/apps/trade/models.py
+++ demo/apps/trade/models.py
@@ -1,3 +1,66 @@
-from django.db import models
+from datetime import datetime
 
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from goods.models import Goods
 # Create your models here.
+
+User = get_user_model()
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, verbose_name='用户')
+    goods = models.ForeignKey(Goods, verbose_name='商品')
+    nums = models.IntegerField(default=0, verbose_name='购买数量')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '购物车'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f'{self.goods.name}({self.nums})'
+
+
+class OrderInfo(models.Model):
+    ORDER_STATUS = (
+        {'success', '成功'},
+        {'cancel', '取消'},
+        {'unpaid', '待支付'},
+    )
+
+    user = models.ForeignKey(User, verbose_name='用户')
+    order_sn = models.CharField(max_length=30, unique=True, verbose_name='订单号')
+    trade_no = models.CharField(max_length=100, null=True, blank=True, unique=True, verbose_name='交易号')
+    pay_status = models.CharField(max_length=10, choices=ORDER_STATUS, verbose_name='订单状态')
+    post_script = models.CharField(max_length=200, verbose_name='订单留言')
+    order_mount = models.FloatField(default=0.0, verbose_name='订单金额')
+    pay_time = models.DateTimeField(null=True, blank=True, verbose_name='支付时间')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    # 用户信息
+    address = models.CharField(max_length=100, default='', verbose_name='收货地址')
+    signer_name = models.CharField(max_length=100, default='', verbose_name='签收人')
+    singer_mobile = models.CharField(max_length=11, verbose_name='联系人电话')
+
+    class Meta:
+        verbose_name = '订单'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.order_sn
+
+
+class OrderGoods(models.Model):
+    order = models.ForeignKey(OrderInfo, verbose_name='订单')
+    goods = models.ForeignKey(Goods, verbose_name='商品')
+    goods_nums = models.IntegerField(default=0, verbose_name='商品数量')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '订单商品'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.order.order_sn
