065a1ba6d9f2abe860025633fcad5801f40f0943
yekingyan <529616@gmail.com>
2019-02-28 23:20:40 +0800

    mixins和generice等View类处理get请求方法， generice.ListApiView

--- demo/apps/goods/views.py
+++ demo/apps/goods/views.py
@@ -1,20 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import (
+    status,
+    mixins,
+    generics,
+)
 
 from .models import Goods
 from .serializers import GoodsSerializer
 # Create your views here.
 
 
-class GoodsListView(APIView):
-    def post(self, request, format=None):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()  # 调用serializers中的create方法
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+class GoodsListView(generics.ListAPIView,
+                    generics.CreateAPIView,):
 
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+    # queryset 和 serializer_class 是必须的
+    queryset = Goods.objects.all()[:10]
+    serializer_class = GoodsSerializer
+
