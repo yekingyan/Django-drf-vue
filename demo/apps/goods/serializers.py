c1cdd70842a1148c3f0574fc858e117c4cc5bf11
yekingyan <529616@gmail.com>
2019-02-28 22:31:56 +0800

    post保存数据，serializers.serializer 的create方法

--- demo/apps/goods/serializers.py
+++ demo/apps/goods/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 
+from goods.models import Goods
+
 
 class GoodsSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, max_length=100)
     click_num = serializers.IntegerField(default=0)
     goods_front_image = serializers.ImageField()
+
+    def create(self, validated_data):
+        # 写入满足上面类变量的validate_data到数据库
+        return Goods.objects.create(**validated_data)
--- demo/apps/goods/views.py
+++ demo/apps/goods/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Goods
 from .serializers import GoodsSerializer
@@ -7,6 +8,11 @@ from .serializers import GoodsSerializer
 
 
 class GoodsListView(APIView):
+    def post(self, request, format=None):
+        serializer = GoodsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # 调用serializers中的create方法
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def get(self, request, format=None):
         goods = Goods.objects.all()[:10]
