83e9b5db279702d03a16d1113ec5f1950bb941be
yekingyan <529616@gmail.com>
2019-03-02 00:40:43 +0800

    完全匹配与条件式的区别

--- demo/apps/goods/filters.py
+++ demo/apps/goods/filters.py
@@ -9,7 +9,9 @@ class GoodsFiter(django_filters.rest_framework.FilterSet):
     """
     price_min = django_filters.NumberFilter(name='shop_price', lookup_expr='gte')
     price_max = django_filters.NumberFilter(name='shop_price', lookup_expr='lte')
+    name = django_filters.CharFilter(name='name', lookup_expr='icontains')
 
     class Meta:
         model = Goods
-        fields = ['price_min', 'price_max']
+        # 需要完成匹配的放进fields中
+        fields = ['id']
